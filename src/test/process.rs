53cce9ec102d12ba81f0d3edcdde6937565abc8b
Devin Jean <emailcruzjean@yahoo.com>
2023-07-25 14:17:39 -0500

    signed zero test

--- src/test/process.rs
+++ src/test/process.rs
@@ -1558,6 +1558,31 @@ fn test_proc_list_json() {
 }
 
 #[test]
+fn test_proc_signed_zero() {
+    let system = Rc::new(StdSystem::new(BASE_URL.to_owned(), None, Config::default(), UtcOffset::UTC));
+    let (mut env, _) = get_running_proc(&format!(include_str!("templates/generic-static.xml"),
+        globals = "",
+        fields = "",
+        funcs = include_str!("blocks/signed-zero.xml"),
+        methods = "",
+    ), Settings::default(), system);
+
+    run_till_term(&mut env, |mc, _, res| {
+        let expect = Value::from_json(mc, json!([
+            [0.0, -0.0, -0.0],
+            ["0", "-0", "-0"],
+            [false, true, true, false, true, false, false],
+            [false, true, true, false, true, false, true],
+            [false, true, true, false, true, false, false],
+            [false, true, true, false, true, false, false],
+            [false, true, true, false, true, false, true],
+            [false, true, true, false, true, false, false],
+        ])).unwrap();
+        assert_values_eq(&res.unwrap().0.unwrap(), &expect, 1e-5, "signed zero");
+    });
+}
+
+#[test]
 fn test_proc_list_combinations() {
     let system = Rc::new(StdSystem::new(BASE_URL.to_owned(), None, Config::default(), UtcOffset::UTC));
     let (mut env, _) = get_running_proc(&format!(include_str!("templates/generic-static.xml"),
